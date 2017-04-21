fc7d0a852f8f54374b3c764b4a767e95a9244d5b
杨庆 <yangqing011@chinasofti.com>
2017-04-21 17:27:02 +0800

    增加生成一个随机不重复的32位UUID字符串（主键）功能

---
+++ src/wx/sunl/util/CreateUUID.java
@@ -0,0 +1,18 @@
+package wx.sunl.util;
+
+import java.util.UUID;
+
+public class CreateUUID {
+	
+	/**
+	 * 生成一个随机不重复的32位UUID字符串
+	 * @return
+	 */
+	public static String getUUID(){
+		UUID uuid = UUID.randomUUID();
+		String str = uuid.toString();
+		// 去掉"-"符号
+		String temp = str.substring(0, 8)+str.substring(9, 13)+str.substring(14, 18)+str.substring(19, 23)+str.substring(24);
+		return temp;
+	}
+}
